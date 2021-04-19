a11511b6b9e373e2d18ccddd710513671facf9aa
Guilherme <Guilherme@DESKTOP-34OFNRN>
2021-04-19 18:28:17 -0300

    Implementa poligono e circulo no mapa

--- frontend/src/app/components/mapas/mapas/mapas.component.ts
+++ frontend/src/app/components/mapas/mapas/mapas.component.ts
@@ -1,4 +1,6 @@
+import { CircleManager } from '@agm/core';
 import { Component, OnInit } from '@angular/core';
+import { LatLng } from './LatLng-model';
 
 @Component({
   selector: 'app-mapas',
@@ -6,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mapas.component.sass']
 })
 export class MapasComponent{
+  constructor(){}
   title = 'Mapa';
   zoom = 15;
   raio = 150;
@@ -17,16 +20,57 @@ export class MapasComponent{
   lngCircle: number;
   circleColor = "#ffcccc"
   circleOpacity = 0
-  goToMcDonalds(){
-    this.latMarker = -19.9375969;
-    this.lngMarker = -43.9353177;
-    this.latCircle = -19.9375969;
-    this.lngCircle = -43.9353177;
+  PcaLiberdadeLat = -19.9319913;
+  PcaLiberdadeLng = -43.9380937;
+  MinasLat = -19.9356743;
+  MinasLng = -43.940647;
+  ConcertLat = -19.937719;
+  ConcertLng = -43.937732;
+  McLat = -19.9375969;
+  McLng = -43.9353177;
+  LifecenterLat = -19.9351079;
+  LifecenterLng = -43.9246347;
+  
+  goToPcaLiberdade(){
+    this.latMarker = this.PcaLiberdadeLat;
+    this.lngMarker = this.PcaLiberdadeLng;
+    this.latCircle = this.PcaLiberdadeLat;
+    this.lngCircle = this.PcaLiberdadeLng;
   }
+
+  goToMinasTenisClube(){
+    this.latMarker = this.MinasLat;
+    this.lngMarker = this.MinasLng;
+    this.latCircle = this.MinasLat;
+    this.lngCircle = this.MinasLng;
+  }
+
   goToConcert(){
-    this.latMarker = -19.937719;
-    this.lngMarker = -43.937732;
-    this.latCircle = -19.937719
-    this.lngCircle = -43.937732
+    this.latMarker = this.ConcertLat;
+    this.lngMarker = this.ConcertLng;
+    this.latCircle = this.ConcertLat;
+    this.lngCircle = this.ConcertLng;
   }
+
+  goToMcDonalds(){
+    this.latMarker = this.McLat;
+    this.lngMarker = this.McLng;
+    this.latCircle = this.McLat;
+    this.lngCircle = this.McLng;
+  }
+
+  goToLifeCenter(){
+    this.latMarker = this.LifecenterLat;
+    this.lngMarker = this.LifecenterLng;
+    this.latCircle = this.LifecenterLat;
+    this.lngCircle = this.LifecenterLng;
+  }
+ 
+  paths: LatLng[] = [
+    {lat: this.PcaLiberdadeLat, lng:this.PcaLiberdadeLng},
+    {lat: this.MinasLat, lng: this.MinasLng},
+    { lat: this.ConcertLat, lng: this.ConcertLng },
+    { lat: this.McLat, lng: this.McLng },
+    { lat: this.LifecenterLat, lng:this.LifecenterLng},
+    ]
 }
